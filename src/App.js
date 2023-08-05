d93ce75a944f270646d9c1784aef7b63ba1d8f2c
keyurdhameliya846 <keyurd846@gmail.com>
2023-08-05 08:42:35 +0530

    keyur

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { increment,decrement,demoapp,incrementByAmount } from './app/reducers/counterSlice';
+import { useDispatch,useSelector } from 'react-redux';
+import { useState } from 'react';
 function App() {
+  const count = useSelector((state) => state.counter.value)
+  const name = useSelector((state) => state.counter.name)
+  const dispatch = useDispatch();
+
+  const [data,setdata] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{count}</h1>
+      <h1>{name}</h1>
+      <input type='button' value="Incerment" onClick={ () => dispatch(increment()) }></input>
+      <input type='button' value="Decerment" onClick={ () => dispatch(decrement()) }></input>
+      <input type='button' value="Demoapp" onClick={ () => dispatch(demoapp()) }></input>
+    <br></br><br></br>
+      <input type='text' onChange={(e) =>{ setdata(e.target.value) }}></input>
+      <input type='button' value="TextVal" onClick={ () => dispatch(incrementByAmount(parseInt(data)))}></input>
+
+
     </div>
   );
 }
